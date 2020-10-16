e363a8d6a16de140a5e4358771a9052d0b9e87c8
fosterv2 <fosterv2@outlook.com>
2020-10-16 11:44:30 -0700

    Vue Introduction: Declarative Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <p>{{ message }}</p>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import App2 from '@/components/App2'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/app-2',
+      name: 'App-2',
+      component: App2
     }
   ]
 })
