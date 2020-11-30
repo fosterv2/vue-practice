260d3f0591169b69edcf830e6d74b38025ccc8e4
fosterv2 <fosterv2@outlook.com>
2020-11-30 13:45:43 -0800

    Test component with v-for

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-    <Book :title='title' :img_url='img_url' :author='author' />
+    <!-- <Book :title='title' :img_url='img_url' :author='author' /> -->
+    <Book
+      v-for='book in books'
+      :key='book.id'
+      :book='book'
+    />
     <router-view/>
   </div>
 </template>
@@ -16,9 +21,14 @@ export default {
   },
   data () {
     return {
-      title: 'The Name of the Wind',
-      img_url: 'https://images-na.ssl-images-amazon.com/images/I/51JThzjy3gL._SX306_BO1,204,203,200_.jpg',
-      author: 'Patrick Rothfuss'
+      books: [
+        {
+          id: 0,
+          title: 'The Name of the Wind',
+          img_url: 'https://images-na.ssl-images-amazon.com/images/I/51JThzjy3gL._SX306_BO1,204,203,200_.jpg',
+          author: 'Patrick Rothfuss'
+        }
+      ]
     }
   }
 }
--- src/components/Book.vue
+++ src/components/Book.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="book">
-    <h3>{{ title }}</h3>
-    <img :src='img_url'>
-    <p>By: {{ author }}</p>
+    <h3>{{ book.title }}</h3>
+    <img :src='book.img_url'>
+    <p>By: {{ book.author }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Book',
+  // props: {
+  //   title: String,
+  //   img_url: String,
+  //   author: String
+  // }
   props: {
-    title: String,
-    img_url: String,
-    author: String
+    book: Object
   }
 }
 </script>
